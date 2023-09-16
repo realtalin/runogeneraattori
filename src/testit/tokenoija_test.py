6adaf7caf49de081fe6709e1497eac5e8408957a
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2023-09-16 21:25:50 +0300

    Lisää yksikkötestit tokenoijalle

---
+++ src/testit/tokenoija_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.tokenoija import puhdista_teksti, tokenoi_teksti
+
+class TestTokenoija(unittest.TestCase):
+    def test_puhdista_teksti(self):
+        teksti = "--Tässä tekstissä on (joitain) turhia ""merkkejä""."
+        puhdistettu = puhdista_teksti(teksti)
+        self.assertEqual(puhdistettu, "Tässä tekstissä on joitain turhia merkkejä.")
+
+    def test_tokenoi_teksti(self):
+        teksti = "Eka lause. Toka lause."
+        tokenoitu = tokenoi_teksti(teksti)
+        self.assertEqual(tokenoitu, [["Eka", "lause", "."], ["Toka", "lause", "."]])
--- src/tokenoija.py
+++ src/tokenoija.py
@@ -4,10 +4,12 @@ import nltk
 sense = nltk.corpus.gutenberg.raw("austen-sense.txt")
 
 def puhdista_teksti(teksti):
-  teksti = re.sub(r'[^\w\s.]', '', teksti)
+  """Puhdistaa tekstitiedoston jättäen sanat, välilyönnit ja lopetusmerkit."""
+  teksti = re.sub(r'[^\w\s.!?]', '', teksti)
   return teksti
 
 def tokenoi_teksti(teksti):
+    """"Tokenoi tekstin. Palauttaa Listan listoja (lauseita), jotka koostuvat sanoista sekä pisteistä."""
     lauseet_sanoina = []
     for lause in nltk.sent_tokenize(teksti):
        lause_sanoina = nltk.word_tokenize(lause)
