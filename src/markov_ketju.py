235bd021e2bfaa1266c128bdb77fdde1b15cd3d8
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2023-09-23 20:05:55 +0300

    Lisää docstringiä

--- src/markov_ketju.py
+++ src/markov_ketju.py
@@ -2,11 +2,13 @@ from src.trie import Trie
 
 class MarkovKetju():
     def __init__(self, korpus: list, taso: int):
+        """Alustaa markov-ketjun. Ketjulle annetaan korpus, joka on lista lauseita, jotka itsessään ovat listoja sanoja, sekä halutun markov-ketjun taso."""
         self.korpus = korpus
         self.sanajonojen_pituus = taso + 1
         self.trie = Trie()
 
     def luo_sanajonot(self):
+        """Luo kaikki sanajonot sille annetusta korpuksesta. Sanajonojen pituus on markov-ketjun taso + 1."""
         sanajonot = []
 
         for lause in self.korpus:
@@ -16,5 +18,6 @@ class MarkovKetju():
         return sanajonot
 
     def generoi_trie(self):
+        """Lisää sanajonot trieen."""
         for sanajono in self.luo_sanajonot():
             self.trie.lisaa(sanajono)
--- src/trie.py
+++ src/trie.py
@@ -1,5 +1,6 @@
 class TrieSolmu:
     def __init__(self):
+        """Luo solmun, jolla on kolme atribuuttia. Attribuutteja muutetaan Trien lisaa-metodissa."""
         self.lapset = {}
         self.on_lehtisolmu = False
         self.frekvenssi = 0
@@ -7,9 +8,11 @@ class TrieSolmu:
 
 class Trie:
     def __init__(self):
+        """Alustaa trien tyhjällä aloitussolmulla."""
         self.aloitussolmu = TrieSolmu()
 
     def lisaa(self, sanajono: list):
+        """Lisää trieen yhden sanajonon, joka on lista sanoja."""
         solmu = self.aloitussolmu
         for sana in sanajono:
             if sana not in solmu.lapset:
