50754bd9d4cac791870b40f191f80fe4984cb5bb
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2023-10-07 23:00:43 +0300

    Paranna käyttöliittymää

--- src/main.py
+++ src/main.py
@@ -2,20 +2,46 @@ import nltk
 from runogeneroija import generoi_ketju, generoi_runo
 
 
+def pyyda_numero(pyynto: str, minimi, maksimi, nolla_lopettaa=False):
+    while True:
+        try:
+            arvo = int(input(pyynto))
+        except ValueError:
+            print("Annettu arvo oli väärän tyyppinen: Anna numero")
+            continue
+
+        if nolla_lopettaa:
+            if arvo == 0:
+                main()
+
+        if arvo < minimi or arvo > maksimi:
+            print(f"Anna numero väliltä {minimi}-{maksimi}")
+            continue
+        break
+
+    return arvo
+
+
 def main():
     teksti = nltk.corpus.gutenberg.raw("austen-sense.txt")
+    taso = pyyda_numero("Monennen tason Markov-ketju? (1-6) ", 1, 6)
+    ketju = generoi_ketju(teksti, taso)
 
-    try:
-        riveja = int(input("Kuinka monta riviä pitkä runo? "))
-        pituus = int(input("Runon rivien maksimipituus? "))
-        taso = int(input("Monennen tason Markov-ketju? "))
+    while True:
+        riveja = pyyda_numero(
+            "Monen rivin runo? (1-100, 0 valitaksesi uusi Markov-ketjun taso) ",
+            1,
+            100,
+            True,
+        )
 
-    except ValueError:
-        print("Annettu parametri oli väärän tyyppinen: Anna numero")
+        rivin_pituus = pyyda_numero(
+            "Rivien maksimipituus? (1-20) ",
+            1,
+            20,
+        )
 
-    else:
-        ketju = generoi_ketju(teksti, taso)
-        runo = generoi_runo(ketju, riveja, pituus)
+        runo = generoi_runo(ketju, riveja, rivin_pituus)
         print(runo)
 
 
