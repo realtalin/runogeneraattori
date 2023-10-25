2419971b406903cb373bf10f1c7984989059febc
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2023-10-25 23:02:11 +0300

    Korjaa runogeneroijatestit

--- src/testit/runogeneroija_test.py
+++ src/testit/runogeneroija_test.py
@@ -1,23 +1,25 @@
 import unittest
 from unittest.mock import MagicMock
-from runogeneroija import generoi_runo
+from runogeneroija import generoi_runo_pituudella
 
 
 class TestRunogeneroija(unittest.TestCase):
     def setUp(self):
         self.ketju = MagicMock()
-        self.ketju.generoi_lause = MagicMock(return_value=["Minä", "olen", "ihminen"])
+        self.ketju.generoi_lause_pituudella = MagicMock(
+            return_value=["Minä", "olen", "ihminen"]
+        )
 
     def test_generoi_yksi_rivi(self):
-        runo = generoi_runo(self.ketju, 1, 3)
+        runo = generoi_runo_pituudella(self.ketju, 1, 3)
 
         self.assertEqual(runo, "\nMinä olen ihminen\n")
-        self.ketju.generoi_lause.assert_called_with(3)
+        self.ketju.generoi_lause_pituudella.assert_called_with(3)
 
     def test_generoi_kolme_rivia(self):
-        runo = generoi_runo(self.ketju, 3, 3)
+        runo = generoi_runo_pituudella(self.ketju, 3, 3)
 
         self.assertEqual(
             runo, "\nMinä olen ihminen\nMinä olen ihminen\nMinä olen ihminen\n"
         )
-        self.ketju.generoi_lause.assert_called_with(3)
+        self.ketju.generoi_lause_pituudella.assert_called_with(3)
